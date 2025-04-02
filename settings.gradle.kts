f8ae1233ceabd40297f724e9fff75c3f217e8847
DeepakRTiwari <deepaktiwari8282@gamil.com>
2025-04-02 18:47:00 +0530

    cleanup - replace module import with dependency import

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,9 +16,10 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
 rootProject.name = "LibGraphy"
 include(":app")
-include(":libGraphy")
+//include(":libGraphy")
